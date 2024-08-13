7ad8f914e349a85c3338ecb611bc55159e99ebc9
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-08-13 00:52:07 -0400

    added email js

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,17 +4,25 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import { validateEmail } from '../utils/helpers';
 
 function contact() {
+
+  const form = useRef();
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
   const [validated, setValidated] = useState(false);
 
   const handleInputChange = (e) => {
+    setSuccessMessage('')
+
     const { target } = e;
     const inputName = target.name;
     const inputValue = target.value;
@@ -29,28 +37,46 @@ function contact() {
   }
 
   const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    event.preventDefault();
+    event.stopPropagation();
 
     if (!validateEmail(email) || !name) {
       setErrorMessage('Email or username is invalid');
+      return;
     }
 
-    if (validateEmail(email) && name && !message) {
+    if (!message) {
       setErrorMessage('Please type a message');
+      return;
     }
 
-
-    setValidated(true);
+    // setValidated(true);
+    setErrorMessage('');
+    sendMail();
+    setSuccessMessage('Message sent successfully!')
+    setName('');
+    setEmail('');
+    setMessage('');
   };
+
+  function sendMail() {
+    emailjs.sendForm('service_8hk4sua', 'template_tx629bl', form.current, {
+      publicKey: 'nDuwMSiDAv-TZV0hx',
+    })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  }
 
   return (
     <div>
       <h2>Contact</h2>
-      <Form noValidate validated={validated} onSubmit={handleSubmit} className='contact' >
+      <Form ref={form} noValidate validated={validated} onSubmit={handleSubmit} className='contact' >
         <Row className="mb-3">
           <Form.Group as={Col} md="12">
             <Form.Label>Name</Form.Label>
@@ -90,10 +116,15 @@ function contact() {
             />
           </Form.Group>
         </Row>
-        <Button type="submit" className='submitBtn'>Submit</Button>
+        <Button type="submit" value="Send" className='submitBtn'>Submit</Button>
         {errorMessage && (
           <div>
             <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        {successMessage && (
+          <div>
+            <p className="error-text" style={{ color: 'green' }}>{successMessage}</p>
           </div>
         )}
       </Form>
@@ -102,3 +133,4 @@ function contact() {
 }
 
 export default contact;
+
